1521127ba5b4b0f5de66e059d5b9bed8fdbd4a22
Yesol Kwon <yeahsol.kwon@gmail.com>
2023-11-21 21:44:40 +1000

    proxy setting

--- src/api/api.js
+++ src/api/api.js
@@ -1,1 +1,14 @@
-export const API_URL = "http://ec2-3-27-193-10.ap-southeast-2.compute.amazonaws.com:8080";
+export const API_URL =
+  "http://ec2-3-27-193-10.ap-southeast-2.compute.amazonaws.com:8080";
+// import axios from "axios";
+
+// const host =
+//   window.location.hostname === "localhost"
+//     ? "http://ec2-3-27-193-10.ap-southeast-2.compute.amazonaws.com:8080"
+//     : "/api";
+
+// export const apiClient = axios.create({
+//   baseURL: host,
+// });
+
+// console.log(apiClient);
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/API_URL":
+        "http://ec2-3-27-193-10.ap-southeast-2.compute.amazonaws.com:8080",
+    },
+  },
   plugins: [react()],
-})
+});
